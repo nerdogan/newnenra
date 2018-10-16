9dd45c41083aa5d504142333341c29074fe0bece
namikerdogan <erdogannamik@gmail.com>
2018-10-16 16:11:35 +0300

    mdb iptal

--- mdb/ui_recete2.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-# Form implementation generated from reading ui file 'recete2.ui'
-#
-# Created: Fri Jun 27 17:09:29 2014
-#      by: PyQt4 UI code generator 4.10.3
-#
-# WARNING! All changes made in this file will be lost!
-
